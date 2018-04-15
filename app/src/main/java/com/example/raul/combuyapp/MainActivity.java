d45ede492fbd11ea4896ab07b0fdb9c3016e83a4
Raulalvaro <raulalvaro123@hotmail.com>
2018-04-15 01:28:04 -0500

    Presentacion y Menu Lateral

--- app/src/main/java/com/example/raul/combuyapp/MainActivity.java
+++ app/src/main/java/com/example/raul/combuyapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.raul.combuyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart, btnLogin,btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btn_Login);
+        btnRegister = (Button) findViewById(R.id.btn_Register);
+        btnStart = (Button) findViewById(R.id.btn_Start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), PrincipalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/raul/combuyapp/PrincipalActivity.java
@@ -0,0 +1,19 @@
+package com.example.raul.combuyapp;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toolbar;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+
+    }
+}
